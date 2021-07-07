542337309f81f353a8eeebb78f5466936cd41031
Junpei-512 <junpei.910512.sunsys.cst@gmail.com>
2021-07-07 10:37:29 +0900

    2021/07/07 初回

---
+++ SubjectTimeTable/LoginView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  SubjectTimeTable
+//
+//  Created by Murayama Junpei on 2021/07/06.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var show: Bool = false
+    var body: some View {
+        NavigationView {
+        VStack{
+            HStack {
+                HStack{
+                    Text("ID:").padding()
+                }
+                TextField(/*@START_MENU_TOKEN@*/"Input your ID"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: 150.0, height: 40.0)
+            }
+            HStack {
+                HStack{
+                    Text("PassWord:").padding()
+                }
+                SecureField(/*@START_MENU_TOKEN@*/"Input your password"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: 150.0, height: 40.0)
+            }
+            NavigationLink(destination: HomeView()) {
+            Button(action:{self.show = true}){
+                Text("→")
+                    .font(.largeTitle)
+                    .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.white)
+                    .background(Color.orange)
+                    .cornerRadius(25, antialiased: true)
+            }
+            .sheet(isPresented: self.$show) {
+                        HomeView()
+                    }
+            }
+        }
+    }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+        HomeView()
+    }
+}
+
---
+++ SubjectTimeTable/SubjectTimeTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubjectTimeTableApp.swift
+//  SubjectTimeTable
+//
+//  Created by Murayama Junpei on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SubjectTimeTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
